49b3ae7057162a48b5948c1d5f1673d6081cba02
Tushar Parmar <tparmar@fulcrum-gt.com>
2018-10-31 18:22:31 -0500

    Initial commit

---
+++ src/test/java/com/tusharparmar/sleuth/sleuthserver/SleuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tusharparmar.sleuth.sleuthserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SleuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
